b03b688db5fbdb2a50b47987a6d30750541ff751
yoonmiao <miaozu1125@126.com>
2023-05-30 15:49:08 +0000

    lab3 checkpoint

--- src/tcp_sender.cc
+++ src/tcp_sender.cc
@@ -40,9 +40,13 @@ void TCPSender::push( Reader& outbound_stream )
     return;
 
   // special case
-  window_size_ = window_size_ ? window_size_ : 1;
-
+  //we just treat a '0' window size as equal to '1' but don't back off RTO
+  //we don't change the actual window_size_
+  
   uint64_t remaining_size = static_cast<uint64_t>( window_size_ ) + recv_ackno_ - next_seqno_;
+  if(window_size_ == 0){
+    remaining_size += 1;
+  }
   // 发送窗口是根据接收端返回的ackno向前移动的，如果recv_ackno不动，发送窗口的起点不能向前移动
   // 计算机网络微课堂讲得很好
 
@@ -94,23 +98,29 @@ TCPSenderMessage TCPSender::send_empty_message() const
 
 void TCPSender::receive( const TCPReceiverMessage& msg )
 {
-  // 没有ackno
-  if ( msg.ackno.has_value() == false )
+  // 没有ackno,就更新下window_size_
+  if ( msg.ackno.has_value() == false ) {
+    window_size_ = msg.window_size;
     return;
+  }
 
   // wrap, unwrap转换得到新的ackno
   auto new_recv_ackno_ = msg.ackno.value().unwrap( isn_, recv_ackno_ );
   window_size_ = msg.window_size;
 
-  //test "Impossible ackno (beyond next seqno) is ignored"
-  //invalid recv_ackno, next_seqno - recv_ackno is invalid, can't get 
-  //correct bytes in flight
-  if (new_recv_ackno_ > next_seqno_ ){
+  // test "Impossible ackno (beyond next seqno) is ignored"
+  // invalid recv_ackno, next_seqno - recv_ackno is invalid, can't get
+  // correct bytes in flight
+  if ( new_recv_ackno_ > next_seqno_ ) { //invalid ackno byond next_seqno
     return;
-  }else{
+  } else if(new_recv_ackno_ <= recv_ackno_){//outdated ackno, too old ackno
+  //test "Repeated ACKs and outdated ACKs are harmless"
+    return;
+  }else {
     recv_ackno_ = new_recv_ackno_;
   }
   
+
   while ( !outstanding_message_.empty() ) {
     auto front_msg = outstanding_message_.front();
     uint64_t front_seqno = front_msg.seqno.unwrap( isn_, recv_ackno_ );
@@ -123,6 +133,11 @@ void TCPSender::receive( const TCPReceiverMessage& msg )
       break;
     }
   }
+
+  //及时关闭timer_
+  if(outstanding_message_.empty()){
+    timer_.stop();
+  }
 }
 
 void TCPSender::tick( const size_t ms_since_last_tick )
@@ -134,7 +149,7 @@ void TCPSender::tick( const size_t ms_since_last_tick )
       consecutive_retransmissions_num_++;
     }
 
-    //maybe_send();面向测试编程
+    // maybe_send();面向测试编程
     timer_.start();
   } else {
     return;
