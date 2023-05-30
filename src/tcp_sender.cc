d2ff4fac1261e2c9a938612201efcf9e7565df0e
yoonmiao <miaozu1125@126.com>
2023-05-30 05:22:28 +0000

    lab3 receive function v1

--- src/tcp_sender.cc
+++ src/tcp_sender.cc
@@ -1,6 +1,6 @@
 #include "tcp_sender.hh"
 #include "tcp_config.hh"
-
+#include "byte_stream.hh"
 #include <random>
 
 using namespace std;
@@ -14,7 +14,7 @@ TCPSender::TCPSender( uint64_t initial_RTO_ms, optional<Wrap32> fixed_isn )
 uint64_t TCPSender::sequence_numbers_in_flight() const
 {
 
-  return outstanding_bytes_;
+  return next_seqno_ - recv_ackno_;
 }
 
 uint64_t TCPSender::consecutive_retransmissions() const
@@ -33,23 +33,86 @@ void TCPSender::push( Reader& outbound_stream )
 {
   if(fin_flag_) return;
 
+  //special case
+  uint16_t window_size_ = window_size_ ? window_size_ : 1;
+
+  uint64_t remaining_size = static_cast<uint64_t>(window_size_) + recv_ackno_ - next_seqno_;
+  //发送窗口是根据接收端返回的ackno向前移动的，如果recv_ackno不动，发送窗口的起点不能向前移动
+  //计算机网络微课堂讲得很好
+
+  //只要发送窗口仍有空间，并且FIN还未发送，就仍然可以读stream中数据push进发送窗口
+  while(remaining_size > 0 && !fin_flag_){
+    TCPSenderMessage msg;
+    //deal with SYN
+    if(!syn_flag_){
+      msg.SYN = true;
+      syn_flag_ = true;
+      remaining_size -=1;
+    }//SYN occupies a sequence number
+
+    //deal with payload
+    msg.seqno = Wrap32::wrap(next_seqno_, isn_);
+    Buffer& buffer = msg.payload;
+    read(outbound_stream, min(remaining_size, TCPConfig::MAX_PAYLOAD_SIZE), buffer.operator std::string &());
+    remaining_size -= buffer.size();
+
+
+    //deal with FIN
+    if(outbound_stream.is_finished() && remaining_size > 0) {
+            msg.FIN = true;
+            fin_flag_ = true;
+            remaining_size -= 1;
+    }
+    
+    //empty segment，没有任何数据，停止数据包的发送
+    if(msg.sequence_length() == 0) return;
+
+    next_seqno_ += msg.sequence_length();
+    outstanding_bytes_ += msg.sequence_length();
+
+    //加入发送队列
+    message_.push_back(msg);
+    if(timer_.is_running() == false){
+      timer_.start();
+    }
+    outstanding_message_.push_back(msg);
+  }
   
 }
 
 TCPSenderMessage TCPSender::send_empty_message() const
 {
-  // Your code here.
-  return {};
+  TCPSenderMessage msg;
+  msg.seqno = Wrap32::wrap(next_seqno_, isn_);
+  return msg;
 }
 
 void TCPSender::receive( const TCPReceiverMessage& msg )
 {
-  // Your code here.
-  (void)msg;
+  //没有ackno
+  if(msg.ackno.has_value() == false) return;
+
+  //wrap, unwrap转换得到新的ackno
+  uint64_t recv_ackno_ = msg.ackno.value().unwrap(isn_, recv_ackno_);
+  window_size_ = msg.window_size;
+
+  if(recv_ackno_ > next_seqno_) return;
+
+  while(!outstanding_message_.empty()){
+    auto front_msg = outstanding_message_.front();
+    uint64_t front_seqno = front_msg.seqno.unwrap(isn_, recv_ackno_);
+    if(recv_ackno_ >= (front_seqno + front_msg.sequence_length()) ){
+      outstanding_message_.pop_front();
+      retransmission_timeout_ = initial_RTO_ms_;
+      timer_.start();
+      consecutive_retransmissions_num_ = 0;
+    }else{
+      break;
+    }
+  }
 }
 
 void TCPSender::tick( const size_t ms_since_last_tick )
 {
-  // Your code here.
-  (void)ms_since_last_tick;
+  
 }
--- src/tcp_sender.hh
+++ src/tcp_sender.hh
@@ -10,10 +10,10 @@ class TCPSender
   uint64_t initial_RTO_ms_;
   uint64_t retransmission_timeout_;//for exponential backoff
 
-  bool sys_flag_ = false;
+  bool syn_flag_ = false;
   bool fin_flag_ = false;
 
-  uint64_t window_size_ = 1;
+  uint16_t window_size_ = 1;
   uint64_t outstanding_bytes_ = 0;
 
   uint64_t consecutive_retransmissions_num_ = 0;
@@ -25,8 +25,10 @@ class TCPSender
   uint64_t next_seqno_ = 0;
 
   // last ackno
-  uint64_t _recv_ackno = 0;
+  uint64_t recv_ackno_ = 0;
 
+  //重传定时器
+  Timer timer_ {};
 
  
 public:
@@ -52,4 +54,27 @@ public:
   /* Accessors for use in testing */
   uint64_t sequence_numbers_in_flight() const;  // How many sequence numbers are outstanding?
   uint64_t consecutive_retransmissions() const; // How many consecutive *re*transmissions have happened?
+};
+
+class Timer{
+private:
+  bool is_running_ = false;
+public:
+  uint64_t ticks_ = 0;
+
+  void start(){
+    is_running_ = true;
+    ticks_ = 0;
+  }
+
+  void stop(){
+    is_running_ = false;
+    ticks_ = 0;
+  }
+
+  bool is_running() const { return is_running_; }
+
+  bool is_expired(uint64_t ms_since_last_tick){
+
+  }
 };
